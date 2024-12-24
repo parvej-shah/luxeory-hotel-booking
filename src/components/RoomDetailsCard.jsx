97ea0dd6c9d8bcb5cfa063e7ea168ce936408836
parvej-shah <parvejshahlabib007@gmail.com>
2024-12-24 23:20:50 +0600

    Room Details page added image gellary

--- src/components/RoomDetailsCard.jsx
+++ src/components/RoomDetailsCard.jsx
@@ -12,42 +12,63 @@ export default function RoomDetailsCard({ roomDetails, images }) {
     inBathroom,
     price,
     reviewCount,
+    available
   } = roomDetails;
+  
   return (
-    <div>
+    <div className="bg-bgStart">
       {/* Room Details Container */}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div className="grid grid-cols-1 container mx-auto bg-bgStart md:grid-cols-2 gap-8 pb-10 pt-4 ">
         {/* Left: Image Gallery */}
         <div>
-          <ImageGallery items={images} showThumbnails={true} />
+          <ImageGallery
+            items={images}
+            showThumbnails={true}
+            renderItem={(item) => (
+              <img
+                src={item.original}
+                className="md:h-[500px] h-[300px] w-full object-cover"
+                alt="original"
+              />
+            )}
+            renderThumbInner={(item) => (
+              <img
+                src={item.thumbnail}
+                className="w-32 h-[80px]"
+                alt="thumbnail"
+              />
+            )}
+          />
         </div>
-
         {/* Right: Room Information */}
-        <div className="space-y-4">
+        <div className="space-y-4 p-4 md:py-0 md:pr-4 text-textPrimary/70">
           {/* Room Title */}
-          <h2 className="text-3xl font-bold">{roomTitle}</h2>
+          <div className={`text-sm font-medium ${!available?"text-red-500 bg-red-200":"text-secondary bg-secondary/10"} w-fit px-2 rounded-full mt-4 md:mt-0`}>
+              {available?"Available":"Booked"}
+            </div>
+          <h2 className="text-3xl font-bold text-primary">{roomTitle}</h2>
 
           {/* Room Size and Bed Type */}
-          <p className="text-gray-600">
+          <p >
             <strong>Room size:</strong> {roomSize}
           </p>
-          <p className="text-gray-600">
+          <p>
             <strong>Bed Type:</strong>{" "}
             {isDouble ? "1 Double Bed" : "Single Bed"}
           </p>
 
           {/* Room Reviews */}
-          <p className="text-gray-600">
+          <p>
             <strong>Comfy beds:</strong> Based on {reviewCount} reviews
           </p>
 
           {/* Room Description */}
-          <p className="text-gray-600">{roomDescription}</p>
+          <p>{roomDescription}</p>
 
           {/* Bathroom Facilities */}
           <div>
-            <h3 className="font-semibold">In your private bathroom:</h3>
-            <ul className="list-disc list-inside text-gray-600">
+            <h3 className="font-semibold text-textPrimary">In your private bathroom:</h3>
+            <ul className="list-disc list-inside">
               {inBathroom?.map((item, index) => (
                 <li key={index}>{item}</li>
               ))}
@@ -56,8 +77,8 @@ export default function RoomDetailsCard({ roomDetails, images }) {
 
           {/* Room Facilities */}
           <div>
-            <h3 className="font-semibold">Facilities:</h3>
-            <ul className="list-disc list-inside text-gray-600">
+            <h3 className="font-semibold text-textPrimary">Facilities:</h3>
+            <ul className="list-disc list-inside">
               {facilities?.map((item, index) => (
                 <li key={index}>{item}</li>
               ))}
@@ -73,7 +94,7 @@ export default function RoomDetailsCard({ roomDetails, images }) {
               onClick={() =>
                 toast.success("Booking functionality coming soon!")
               }
-              className="btn btn-primary"
+              className="btn bg-secondary/90 border-none text-white font-bold px-6 py-3 rounded-lg hover:bg-secondary"
             >
               Let&apos;s Book Now
             </button>
--- src/pages/RoomDetails.jsx
+++ src/pages/RoomDetails.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
-import { useNavigate, useParams } from "react-router-dom";
+import {useParams } from "react-router-dom";
 import API from "../hooks/useAPI";
 import LoadingClip from "../components/LoadingClip";
 import { toast } from "react-toastify";
@@ -8,10 +8,10 @@ import "react-image-gallery/styles/css/image-gallery.css";
 import RoomDetailsCard from "../components/RoomDetailsCard";
 
 export default function RoomDetails() {
-  const navigate = useNavigate();
   const { id } = useParams();
   const [images, setImages] = useState([]);
-
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(null);
   // Fetch Room Details
   const getRoomDetails = async () => {
     try {
@@ -45,7 +45,7 @@ export default function RoomDetails() {
   return (
     <div className="container mx-auto">
         <RoomDetailsCard roomDetails={roomDetails} images={images}/>
-      
+        
     </div>
   );
 }
