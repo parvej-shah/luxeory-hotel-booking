22972ac3d131ab5e726a0f7b38fc565c6f90c9ca
parvej-shah <parvejshahlabib007@gmail.com>
2025-01-11 11:03:53 +0600

    add HotelMap component to Contact Us page and adjust layout for improved styling

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,16 +1,17 @@
 import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa';
+import HotelMap from '../components/Map';
 
 const ContactUs = () => {
     return (
         <section className="py-12">
-            <div className="container mx-auto px-6 lg:px-16">
+            <div className="container mx-auto px-6 lg:px-16 mb-6">
                 <h2 className="text-4xl font-bold text-center text-gray-800 dark:text-white mb-8">
                     Get In Touch
                 </h2>
                 <p className="text-center text-gray-600 dark:text-gray-400 mb-12">
-                    We'd love to hear from you! Please fill out the form below and we'll get back to you as soon as possible.
+                    We&apos;d love to hear from you! Please fill out the form below and we&apos;ll get back to you as soon as possible.
                 </p>
-                <div className="flex flex-col lg:flex-row gap-12">
+                <div className="flex flex-col-reverse lg:flex-row gap-12">
                     {/* Contact Form */}
                     <div className="lg:w-1/2">
                         <form className="bg-bgStart shadow-lg rounded-lg p-6 space-y-4">
@@ -81,6 +82,7 @@ const ContactUs = () => {
                     </div>
                 </div>
             </div>
+            <HotelMap/>
         </section>
     );
 };
